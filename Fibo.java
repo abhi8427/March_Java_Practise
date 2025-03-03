28847ae99084a47baeeab2f21d56661339489541
Abhishek <abhishek.coolabhi@gmail.com>
2025-03-03 13:11:15 +0530

    fibonicci series

---
+++ Fibo.java
@@ -0,0 +1,32 @@
+package March;
+
+import java.util.Scanner;
+
+public class Fibo {
+	
+	public static void main(String ab[]){
+		
+		fiboSeries();
+		
+	}
+	
+	
+	public static void fiboSeries(){
+		System.out.println("Please enter the terms");
+		Scanner scan = new Scanner(System.in);
+		int term = scan.nextInt();
+		System.out.println("You want to generate Fibonici Series upto "+ term + "th term");
+		System.out.println("Below is the generated fibonici series");
+		
+		int n1 = 0; int n2 = 1; int n3;
+		System.out.println(n1);
+		System.out.println(n2);
+		for(int i = 1;i<=term-2;i++){
+			n3 = n1+n2;
+			System.out.println(n3);
+			n1 = n2;
+			n2 = n3;
+		}
+	}
+
+}
